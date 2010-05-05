5e8186b19d309d2423e4a50c284a1408e7624152
Juan A. Suarez Romero <jasuarez@igalia.com>
2010-05-05 21:32:43 +0200

    Update introspection
    Add latest specification changes (see
    http://live.gnome.org/Rygel/MediaServerSpec).

--- lib/media-server1-introspection.h
+++ lib/media-server1-introspection.h
@@ -33,28 +33,43 @@
 
 #define MEDIAOBJECT1_IFACE                                              \
   "  <interface name=\"org.gnome.UPnP.MediaObject1\">"                  \
-  "    <property name=\"DisplayName\" type=\"s\" access=\"read\"/>"     \
   "    <property name=\"Parent\"      type=\"o\" access=\"read\"/>"     \
+  "    <property name=\"Type\"        type=\"s\" access=\"read\"/>"     \
   "    <property name=\"Path\"        type=\"o\" access=\"read\"/>"     \
+  "    <property name=\"DisplayName\" type=\"s\" access=\"read\"/>"     \
   "  </interface>"
 
 #define MEDIAITEM1_IFACE                                                \
   "  <interface name=\"org.gnome.UPnP.MediaItem1\">"                    \
-  "    <property name=\"Album\"    type=\"s\"  access=\"read\"/>"       \
-  "    <property name=\"Artist\"   type=\"s\"  access=\"read\"/>"       \
-  "    <property name=\"Bitrate\"  type=\"i\"  access=\"read\"/>"       \
-  "    <property name=\"Duration\" type=\"i\"  access=\"read\"/>"       \
-  "    <property name=\"Genre\"    type=\"s\"  access=\"read\"/>"       \
-  "    <property name=\"Height\"   type=\"i\"  access=\"read\"/>"       \
-  "    <property name=\"MIMEType\" type=\"s\"  access=\"read\"/>"       \
-  "    <property name=\"Type\"     type=\"s\"  access=\"read\"/>"       \
-  "    <property name=\"URLs\"     type=\"as\" access=\"read\"/>"       \
-  "    <property name=\"Width\"    type=\"i\"  access=\"read\"/>"       \
+  "    <property name=\"URLs\"           type=\"as\" access=\"read\"/>" \
+  "    <property name=\"MIMEType\"       type=\"s\"  access=\"read\"/>" \
+  "    <property name=\"Size\"           type=\"i\"  access=\"read\"/>" \
+  "    <property name=\"Artist\"         type=\"s\"  access=\"read\"/>" \
+  "    <property name=\"Album\"          type=\"s\"  access=\"read\"/>" \
+  "    <property name=\"Date\"           type=\"s\"  access=\"read\"/>" \
+  "    <property name=\"Genre\"          type=\"s\"  access=\"read\"/>" \
+  "    <property name=\"DLNAProfile\"    type=\"s\"  access=\"read\"/>" \
+  "    <property name=\"Duration\"       type=\"i\"  access=\"read\"/>" \
+  "    <property name=\"Bitrate\"        type=\"i\"  access=\"read\"/>" \
+  "    <property name=\"SampleRate\"     type=\"i\"  access=\"read\"/>" \
+  "    <property name=\"BitsPerSample\"  type=\"i\"  access=\"read\"/>" \
+  "    <property name=\"Width\"          type=\"i\"  access=\"read\"/>" \
+  "    <property name=\"Height\"         type=\"i\"  access=\"read\"/>" \
+  "    <property name=\"ColorDepth\"     type=\"i\"  access=\"read\"/>" \
+  "    <property name=\"PixelWidth\"     type=\"i\"  access=\"read\"/>" \
+  "    <property name=\"PixelHeight\"    type=\"i\"  access=\"read\"/>" \
+  "    <property name=\"Thumbnail\"      type=\"o\"  access=\"read\"/>" \
+  "    <property name=\"AlbumArt\"       type=\"o\"  access=\"read\"/>" \
   "  </interface>"
 
 #define MEDIACONTAINER1_IFACE                                           \
   "  <interface name=\"org.gnome.UPnP.MediaContainer1\">"               \
-  "    <method name=\"ListObjects\">"                                   \
+  "    <property name=\"Items\"          type=\"ao\" access=\"read\"/>" \
+  "    <property name=\"ItemCount\"      type=\"u\"  access=\"read\"/>" \
+  "    <property name=\"Containers\"     type=\"ao\" access=\"read\"/>" \
+  "    <property name=\"ContainerCount\" type=\"u\"  access=\"read\"/>" \
+  "    <property name=\"Searchable\"     type=\"b\"  access=\"read\"/>" \
+  "    <method name=\"ListChildren\">"                                  \
   "      <arg name=\"offset\"  direction=\"in\"  type=\"u\"/>"          \
   "      <arg name=\"max\"     direction=\"in\"  type=\"u\"/>"          \
   "      <arg name=\"filter\"  direction=\"in\"  type=\"as\"/>"         \
